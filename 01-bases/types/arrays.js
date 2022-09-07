d836430142c2b168cf8bb2b9e052e7ba6084a099
Fabian Portilla <fas_456@hotmail.com>
2022-09-06 21:55:42 -0500

    Arrays

---
+++ 01-bases/types/arrays.js
@@ -0,0 +1,9 @@
+"use strict";
+(() => {
+    const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    //    numbers.push('true')
+    //    numbers.push(11)
+    //   numbers.push(true)
+    const villains = ['Omega red', 'Dormammu', 'Green goblin'];
+    villains.forEach(v => console.log(v.toUpperCase()));
+})();
---
+++ 01-bases/types/arrays.ts
@@ -0,0 +1,13 @@
+(() => {
+
+    const numbers: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+    //    numbers.push('true')
+    //    numbers.push(11)
+    //   numbers.push(true)
+
+    const villains: string[] = ['Omega red', 'Dormammu', 'Green goblin'];
+
+    villains.forEach(v => console.log(v.toUpperCase()));
+
+})()
