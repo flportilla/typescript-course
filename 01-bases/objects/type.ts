c84cf231f1d56017223f1896bdf2dbd57134c60b
Fabian Portilla <fas_456@hotmail.com>
2022-09-07 07:46:49 -0500

    Type

---
+++ 01-bases/objects/type.ts
@@ -0,0 +1,26 @@
+(() => {
+
+    type Hero = {
+        name: string;
+        age?: number;
+        powers: string[];
+        getName?: () => string;
+    }
+
+    let flash: Hero = {
+        name: 'Barry Allen',
+        age: 24,
+        powers: ['speed', 'time travel']
+    };
+    let superman: Hero = {
+        name: 'Clark',
+        age: 60,
+        powers: ['speed', 'time travel'],
+        getName() {
+            return this.name
+        }
+    };
+
+
+
+})()
