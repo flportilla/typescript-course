63e429f8ee1cbd5ec798516e7db3f128f29c7227
Fabian Portilla <fas_456@hotmail.com>
2022-09-06 22:52:22 -0500

    Rest arguments

---
+++ 01-bases/functions/args-rest.js
@@ -0,0 +1,9 @@
+"use strict";
+(() => {
+    //Only the first argument is mandatory
+    const fullName = (firstName, ...restArgs) => {
+        return `${firstName} ${restArgs.join(' ')}`;
+    };
+    const superman = fullName('Clark', 'Joseph', 'Kent');
+    console.log({ superman });
+})();
---
+++ 01-bases/functions/args-rest.ts
@@ -0,0 +1,10 @@
+(() => {
+
+    //Only the first argument is mandatory
+    const fullName = (firstName: string, ...restArgs: string[]) => {
+        return `${firstName} ${restArgs.join(' ')}`
+    }
+
+    const superman = fullName('Clark', 'Joseph', 'Kent')
+    console.log({ superman })
+})()
