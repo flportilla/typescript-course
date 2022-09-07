04b2caf2f6e03b69a81e9d26c41f7c7c18d428ea
Fabian Portilla <fas_456@hotmail.com>
2022-09-06 22:05:17 -0500

    enums

--- 01-bases/types/enums.js
+++ 01-bases/types/enums.js
@@ -4,6 +4,7 @@
     (function (AudioLevel) {
         AudioLevel[AudioLevel["min"] = 1] = "min";
         AudioLevel[AudioLevel["medium"] = 2] = "medium";
+        AudioLevel[AudioLevel["medium2"] = 3] = "medium2";
         AudioLevel[AudioLevel["max"] = 10] = "max";
     })(AudioLevel || (AudioLevel = {}));
     let currentAudio = AudioLevel.max;
