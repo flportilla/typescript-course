3d88b5f05597e8b6b384ae40754fe6526139830e
Fabian Portilla <fas_456@hotmail.com>
2022-09-06 21:59:29 -0500

    Tuples (only in ts)

---
+++ 01-bases/types/tuples.js
@@ -0,0 +1,9 @@
+"use strict";
+(() => {
+    const hero = ['Dr. Strange', 100, true];
+    //not allowed on ts
+    //hero[0] = 50
+    //hero[1] = '100'
+    //hero[2] = 123
+    console.log(hero);
+})();
---
+++ 01-bases/types/tuples.ts
@@ -0,0 +1,12 @@
+(() => {
+
+    const hero: [string, number, boolean] = ['Dr. Strange', 100, true]
+
+    //not allowed on ts
+    //hero[0] = 50
+    //hero[1] = '100'
+    //hero[2] = 123
+
+    console.log(hero)
+
+})()
