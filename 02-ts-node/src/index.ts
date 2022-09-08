06858793465ae5098f5e4987af7d6e002e4c6dde
Fabian Portilla <fas_456@hotmail.com>
2022-09-08 08:49:05 -0500

    Classes

---
+++ 02-ts-node/src/index.ts
@@ -0,0 +1,11 @@
+import { Hero } from "./classes/Hero";
+// import * as HeroClasses from './classes/Hero'
+// import powers from './data/powers'
+
+// const Hero = 123
+
+// const ironman = new SuperHero('Ironman', 1, 55)
+const ironman = new Hero('Ironman', 10, 55)
+
+console.log(ironman)
+console.log(ironman.power)
