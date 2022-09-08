76facb677ad33616d8f6e5d022fbfd3f17307afd
Fabian Portilla <fas_456@hotmail.com>
2022-09-08 10:44:04 -0500

    Decorators

--- 02-ts-node/src/index.ts
+++ 02-ts-node/src/index.ts
@@ -1,6 +1,15 @@
-import { getPokemon } from "./generics/get-pokemon"
+// import { getPokemon } from "./generics/get-pokemon"
 
-getPokemon(1)
-    .then(pokemon => console.log(pokemon.sprites))
-    .catch(error => console.error(error))
-    .finally(() => console.log('end'))
+// getPokemon(1)
+//     .then(pokemon => console.log(pokemon.sprites))
+//     .catch(error => console.error(error))
+//     .finally(() => console.log('end'))
+
+import { Pokemon } from "./decorators/pokemon";
+
+const charmander = new Pokemon('charmander')
+
+// (Pokemon.prototype as any).customName = 'pok'
+// charmander.savePokemonToDB(0)
+
+console.log(charmander)
